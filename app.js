88e1b8890e49d626a917fc1cd6b2edd1d77102ab
jeje-test <ngojer@gmail.com>
2025-03-14 22:41:28 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function onScanSuccess(decodedText) {
+        document.getElementById("result").innerText = `QR Code: ${decodedText}`;
+        sendToGoogleSheet(decodedText);
+    }
+
+    new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 }).render(onScanSuccess);
+});
+
+function sendToGoogleSheet(data) {
+    fetch("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ qrData: data })
+    }).then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.error("Erreur:", error));
+}
