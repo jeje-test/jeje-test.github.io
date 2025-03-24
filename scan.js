190c4b3d7ac1884f4242a23cdb745515f741b05d
jeje-test <ngojer@gmail.com>
2025-03-24 15:34:52 +0100

    Update scan.js

--- scan.js
+++ scan.js
@@ -7,8 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const stopScanButton = document.getElementById("stopScan");
     const versionDiv = document.getElementById("appVersion");
 
-    const scriptURL = "https://script.google.com/macros/s/AKfycbx00xnHZMVdT3TOTdL_An7qM1AWmbAcHQK2lvmpgrdomX5s0WoPD3GK_FH0OaqCtXuB/exec?action=getData&q=";
-
+    const scriptURL = "https://script.google.com/macros/s/AKfycbx573h17nUKgmFA1V0iZdDQRXV_V8BpJTeH2oOZY3b9WRwC5xyQlUhEX__pwrjPBq_R/exec?action=getData&q=";
     let html5QrCode = null;
 
     // Fonction pour récupérer la version
